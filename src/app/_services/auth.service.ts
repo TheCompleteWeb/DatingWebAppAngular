2b257693c6ea346e0fe8689182161226277f6ac0
neogeoss <neogeo-s@hanmail.net>
2018-03-27 21:16:51 +0900

    Angular JWT  to hanlde token safely

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,6 +4,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { Observable } from 'rxjs/Observable';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +12,10 @@ export class AuthService {
     userToken: any;
 
     constructor(private http: Http) { }
+
+
+
+
     login(model: any) {
         const headers = new Headers({'Content-type': 'application/json'});
         const options = new RequestOptions({headers: headers});
@@ -25,6 +30,10 @@ export class AuthService {
 
     register(model: any) {
         return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
+    }
+
+    loggedIn() {
+        return tokenNotExpired('token');
     }
 
     private requestOptions() {
