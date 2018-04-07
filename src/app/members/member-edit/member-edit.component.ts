be0f7830352b5e2c3d0e0c94efdb3e08a911cb48
neogeoss <neogeo-s@hanmail.net>
2018-04-07 09:44:24 +0900

    Page Reset, alert, and few addtional changes added to edit page

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../_model/User';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from '../../_services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,13 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  @ViewChild('editForm') editForm: NgForm;
   
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success("Profile Updated successfully");
+    this.editForm.reset(this.user);
   }
 
 }
