84742ca4d9c5ef90f80a94cf29400889a4d46dff
neogeoss <neogeo-s@hanmail.net>
2018-04-12 00:30:21 +0900

    Photo uploader 3rd party component added to the profile edit page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 
 
@@ -55,7 +56,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     RouterModule.forRoot(appRoutes),
     AuthModule,
     TabsModule.forRoot(),
-    NgxGalleryModule
+    NgxGalleryModule,
+    FileUploadModule
   ],
   providers: [
     AuthService,
--- src/app/members/photo-editor/photo-editor.component.ts
+++ src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from '../../_model/Photo';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,10 +9,17 @@ import { Photo } from '../../_model/Photo';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
+  uploader: FileUploader = new FileUploader({});
+
+  hasBaseDropZoneOver = false;
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
 }
